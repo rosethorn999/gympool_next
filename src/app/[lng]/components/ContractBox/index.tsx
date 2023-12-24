b1165b17a739626fe535c6b8a84632398a83e62d
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2023-12-24 13:02:24 -0800

    ci: Run manually

--- src/app/[lng]/components/ContractBox/index.tsx
+++ src/app/[lng]/components/ContractBox/index.tsx
@@ -1,8 +1,19 @@
 import { useTranslation } from '../../../i18n';
 import { ContractBase } from './ContractBase';
+import { Contract } from '@/app/types/type';
 
-export const ContractBox = async ({ lng, fitXs = false, r }) => {
+async function ContractBox({
+	lng,
+	fitXs = false,
+	r,
+}: {
+	lng: string;
+	fitXs: boolean;
+	r: Contract;
+}) {
 	// eslint-disable-next-line react-hooks/rules-of-hooks
 	const { t } = await useTranslation(lng, 'contracts');
+
 	return <ContractBase t={t} r={r} fitXs={fitXs} />;
-};
+}
+export default ContractBox;
